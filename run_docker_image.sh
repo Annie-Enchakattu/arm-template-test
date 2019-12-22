f7d7a420341f18c6d9a2fcc05bf50964907789b6
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-22 14:01:52 -0800

    Update run_docker_image.sh

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,7 +1,8 @@
-git clone https://github.com/microsoft/landcover.git
-cd landcover
-git checkout feature/vm-arm-template
-cd ARM/simple_docker_test
+#git clone https://github.com/microsoft/landcover.git
+#cd landcover
+#git checkout feature/vm-arm-template
+#cd ARM/simple_docker_test
 
-sudo docker build . -t simple-docker-test:1
-sudo docker run "simple-docker-test:1"
+#sudo docker build . -t simple-docker-test:1
+#sudo docker run "simple-docker-test:1"
+echo hello
