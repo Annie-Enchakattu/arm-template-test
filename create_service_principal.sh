9ce68c7628bccb2bfa308e6364762aa329fcc932
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-23 14:28:26 -0800

    Update create_service_principal.sh

--- create_service_principal.sh
+++ create_service_principal.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-# create service principal required for de-allocating VM
-
-#first make user create the service principal, have the enter it in ARM ?
-
 #az cli script for installing ARM template - in progress
 read -p "Enter name for service principal user (required for de-allocating VM after the tasks are complete): " serviceprincipal_name
 read -p "Enter your subscriptionId: " subID
@@ -28,12 +23,3 @@ echo "Save the following details you will need this information in the create VM
 echo "appId: $appID"
 echo "tenantId: $tenantId"
 echo "password: $password"
-
-#add ARM template in github
-#load ARM template from github
-#pass parameters to the template
-#try the custom script using bash script for running docker image as docker extension option in ARM for linux is deprecated
-#custom bash script pull a docker image and run
-
-# #test=$(az login --service-principal -u $appID --password $password --tenant $tenant --query [0:2]['tenantId','name','password'] -o json)
-#az vm deallocate -g $resource_group -n $vm_name
