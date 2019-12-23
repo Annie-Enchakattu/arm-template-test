43f65e8162407a4a3221b845fb1017d6c329a290
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-22 16:53:55 -0800

    Update build_run_docker_image.sh

--- build_run_docker_image.sh
+++ build_run_docker_image.sh
@@ -11,5 +11,6 @@ AZ_RESOURCE_GRP="$1"
 AZ_RESOURCE_NAME="$2"
 EOF
 
+sleep 10m
 sudo docker build . -t simple-docker-test:1
 sudo docker run --name test --mount type=bind,source="$(pwd)"/logs,target=/app/logs "simple-docker-test:1"
